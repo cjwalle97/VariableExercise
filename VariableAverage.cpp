74ccca155c09ae5e3b50d83122949bc0610c4112
cjwalle97 <cjwalle97@gmail.com>
2016-08-30 14:33:56 -0500

    Add files via upload

---
+++ VariableAverage.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+//Name: average
+//Type: int
+//Description: allow user to input five numbers before calculating the average
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	std::cin >> a >> b >> c >> d >> e;
+	int sum;
+	int average = sum / 5;
+	std::cout << average << std::endl;
+	system("pause");
+	return 0;
+}
