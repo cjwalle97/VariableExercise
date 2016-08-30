a4ab86d079838f7c7368c7048f7f2a9e3296b105
cjwalle97 <cjwalle97@gmail.com>
2016-08-30 13:34:27 -0500

    Add files via upload

--- Value.cpp
+++ Value.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 //Find the value of each of the following expressions, or explain why it is not a valid
 //expression.Try this without coding it first.
-//a) 2 / 3 + 3 / 5 
-//b) 2 % 3 + 3 % 5 
-//c) 23 % 15 % -2 
-//d) 25 * 1 / 2 
-//e) 25 * 1.0 / 2 
-//f) 25 * (1 / 2)
-//g) 235 / 8 + 7
-//h) 235 / 8.0 + 7
-//i) ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2int main(){	int result = 2 / 3 + 3 / 5;	std::cout << result << std::endl;	system("pause");}
+//a) 2 / 3 + 3 / 5 Not Valid: both numbers equate to 0 due to them being integers
+//b) 2 % 3 + 3 % 5 Value: 5
+//c) 23 % 15 % -2 Not Valid: negative number
+//d) 25 * 1 / 2 Value: 12
+//e) 25 * 1.0 / 2 Value: 12
+//f) 25 * (1 / 2) Not Valid (1/2) equates to 0
+//g) 235 / 8 + 7 value: 36
+//h) 235 / 8.0 + 7 value: 36
+//i) ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2 not valid: 2-2.0 = 0
+int main()
+{
+	int result = 2 / 3 + 3 / 5;
+	std::cout << result << std::endl;
+	system("pause");
+	return 0;
+}
