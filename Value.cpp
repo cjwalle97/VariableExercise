152494cc2b1b038739ea778417f0cd74c38ffcdf
cjwalle97 <cjwalle97@gmail.com>
2016-08-29 19:23:12 -0500

    Add files via upload

---
+++ Value.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//Find the value of each of the following expressions, or explain why it is not a valid
+//expression.Try this without coding it first.
+//a) 2 / 3 + 3 / 5 
+//b) 2 % 3 + 3 % 5 
+//c) 23 % 15 % -2 
+//d) 25 * 1 / 2 
+//e) 25 * 1.0 / 2 
+//f) 25 * (1 / 2)
+//g) 235 / 8 + 7
+//h) 235 / 8.0 + 7
+//i) ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2int main(){	int result = 2 / 3 + 3 / 5;	std::cout << result << std::endl;	system("pause");}
